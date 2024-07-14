4d626bd7774cffbd1468d0fb5892f9f27a546623
MDshirzad <mohamadshirzad81@gmail.com>
2024-07-14 15:01:44 +0330

    Add repo

---
+++ Infrastructure/DbContext/FakeTodolistRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace TodolistBackend;
+
+public class FakeTodolistRepository
+{
+
+     private static List<Todo> _instance;    
+
+ 
+
+    public static  List<Todo>  Todos
+    {
+        get
+        {
+            if(_instance == null)
+                _instance = new  List<Todo> ();
+
+            return _instance;
+        }
+    }
+
+}
